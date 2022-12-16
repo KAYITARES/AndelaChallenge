17ca7093bc67752e2f0c3df14169f306ce36c789
KAYITARES <cyntkayitare@gmail.com>
2022-12-16 22:22:38 +0200

    design of andela challenge

---
+++ index.js
@@ -0,0 +1,7 @@
+let btnTransaction=document.querySelector('.btn-transaction');
+let modal=document.querySelector('.modal');
+
+btnTransaction.addEventListener('click',()=>{
+    modal.style.display='block';
+   
+})
